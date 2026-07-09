b5515674c87c32365bc72a9f9ea174d93abe2f05
alammaazui <fwebdev2021@gmail>
2026-07-09 23:08:59 +0500

    n

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require("sequelize");
 const mysql2 = require('mysql2'); 
-
+require('dotenv').config()
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
+    port : process.env.DB_PORT, 
     dialectModule: mysql2,
+    dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // fine for dev; use proper CA cert in production
+    }
+  }
 });
 
 module.exports = sequelize
