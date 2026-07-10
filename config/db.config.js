868d5fbc9cc681095a32a051197be97c2cb3d272
maazcreativelab <historicalcontentcreator2023@gmail.com>
2026-07-10 23:26:26 +0500

    f

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require("sequelize");
-
+const mysql2 = require('mysql2'); 
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql"
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectModule: mysql2,
 });
 
 module.exports = sequelize
